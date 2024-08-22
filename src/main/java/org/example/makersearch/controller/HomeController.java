5df37b59b9ae839a659f8f6bebbd9b8ffd691ae8
v_code <12273445-v_code@users.noreply.gitlab.com>
2024-08-22 14:19:39 +0300

    Initial commit

---
+++ src/main/java/org/example/makersearch/controller/HomeController.java
@@ -0,0 +1,18 @@
+package org.example.makersearch.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * @author v_code
+ **/
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    @ResponseBody
+    public String home() {
+        return "Welcome to Maker-Search!";
+    }
+}
---
+++ src/test/java/org/example/makersearch/MakerSearchApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.makersearch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MakerSearchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
